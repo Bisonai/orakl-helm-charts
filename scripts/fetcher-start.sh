d333eaa6bcfbd9df85f3b09c62474b7f1d15b308
jo <jo@bisonai.com>
2023-06-05 10:52:53 +0900

    update secret manager useage:

--- scripts/fetcher-start.sh
+++ scripts/fetcher-start.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-yarn cli fetcher start --chain baobab --id 0xf4e664cc5b6cf7006e3d3db842095d175d46444e2f92b1259ab8a98899598a56
+yarn cli fetcher start --chain baobab --id 0xbb42e0a9c45b898b29b763e28c2aaf3c117fb638fd8035378181b7cd6b613bd3
+yarn cli fetcher start --chain baobab --id 0xdbb1237a135711c5c05bddd02cf283c1025f27a8b069a7eeaa19afa9d4974a7d
+yarn cli fetcher start --chain baobab --id 0x0a83a96b17bf88b71054313870f9c23bd9a8f4a46f13bb1befe3102dd1a7fbae
+yarn cli fetcher start --chain baobab --id 0xa820b5a019e4e7eb785f541466190fe78d67cc8410ef39391f5b35deedc75cc9
 yarn cli fetcher start --chain baobab --id 0x249431dc3ec697ad2324478289ee40be941d8c7d5d7a3f77f8aef14e200ae3a5
 yarn cli fetcher start --chain baobab --id 0x1ba663139938b66528d1b8f28a89db819be1e445fc0f4c0d137f83a273e5f6a5
 yarn cli fetcher start --chain baobab --id 0xc3d7ff282b7ecdc26b72382546154edafbff90d37e171fa80db5ccd6eb94c0f4
 yarn cli fetcher start --chain baobab --id 0xc9a02ddb5c877dc9806393382b633a072dea171786cbe895ee77130c420710e9
 yarn cli fetcher start --chain baobab --id 0x8a316f999ab52280460671482176173f9e2766cae8983f84e277a003f79a0df7
-yarn cli fetcher start --chain baobab --id 0x0a83a96b17bf88b71054313870f9c23bd9a8f4a46f13bb1befe3102dd1a7fbae
-yarn cli fetcher start --chain baobab --id 0xa820b5a019e4e7eb785f541466190fe78d67cc8410ef39391f5b35deedc75cc9
-yarn cli fetcher start --chain baobab --id 0xbb42e0a9c45b898b29b763e28c2aaf3c117fb638fd8035378181b7cd6b613bd3
-yarn cli fetcher start --chain baobab --id 0x6299029414ddcff77d22a2145a8666df212f2c3813914292ed459de223b97b7c
-yarn cli fetcher start --chain baobab --id 0xdbb1237a135711c5c05bddd02cf283c1025f27a8b069a7eeaa19afa9d4974a7d
+yarn cli fetcher start --chain baobab --id 0x5c7e2bfe50cc780ede1769dd166a8e1840dca5c971bf4f3b659ff0eaa6eae7e6
+yarn cli fetcher start --chain baobab --id 0xe9d72126d3d2017757f2c44ccaaec3ea541e043e0fb976f04ce900d857481d92
+yarn cli fetcher start --chain baobab --id 0x4f864798f40a3ad4163400b31d493cf9cf03aa4dd9ce1153b9bda5e47cf6ecaa
